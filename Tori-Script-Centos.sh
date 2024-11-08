6ceb563bb1d26961762510216f706bbecfbb0e30
Sahith Krishna Palla <sahith@kdexglobal.com>
2024-11-08 11:24:33 -0600

    Updated script files

--- Tori-Script-Centos.sh
+++ Tori-Script-Centos.sh
@@ -7,10 +7,10 @@ set -eu
 if ! command -v ibmcloud &> /dev/null
 then
     echo "IBM Cloud CLI is not installed. Installing IBM Cloud CLI..."
-    
+
     # Download and install IBM Cloud CLI
     curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-    
+
     echo "IBM Cloud CLI installation completed."
 else
     echo "IBM Cloud CLI is already installed."
@@ -31,26 +31,20 @@ echo "IBM Cloud login successful."
 # Log into IBM Container Registry with Docker
 echo "Logging into IBM Cloud Container Registry..."
 ibmcloud plugin install container-registry
+ibmcloud plugin list
 ibmcloud cr login
 
 # Check if Docker is installed
 if ! command -v docker &> /dev/null
 then
     echo "Docker is not installed. Installing Docker..."
-    
-    # Install Docker based on the OS type (Linux/macOS)
-    if [ "$(uname)" == "Linux" ]; then
-        # On Linux systems (Debian/Ubuntu)
-        sudo apt-get update
-        sudo apt-get install -y docker.io
-        sudo systemctl start docker
-        sudo systemctl enable docker
-    elif [ "$(uname)" == "Darwin" ]; then
-        # On macOS
-        brew install --cask docker
-        open /Applications/Docker.app
-        echo "Please ensure Docker is running on macOS."
-    fi
+
+    # Install Docker on CentOS
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
 else
     echo "Docker is already installed."
 fi
@@ -59,12 +53,12 @@ fi
 if ! command -v docker-compose &> /dev/null
 then
     echo "Docker Compose is not installed. Installing Docker Compose..."
-    
+
     # Install Docker Compose
     COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
     sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    
+
     echo "Docker Compose installation completed."
 else
     echo "Docker Compose is already installed."
@@ -86,6 +80,7 @@ deactivate
 # Stop and remove Docker containers
 docker-compose down
 
+# Pull the latest images
 docker-compose pull
 
 # Run Docker Compose
